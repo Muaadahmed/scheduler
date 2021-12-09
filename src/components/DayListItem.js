b9c5c0391df2eb6f494bae767c0855dd3d0603bb
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2021-12-09 10:44:01 -0500

    Import days array and Daylist component in Application component and display dayList in nav tag

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -14,14 +14,14 @@ export default function DayListItem(props) {
             return `no spots remaining`;
         }
 
-        return `${prop.spots} spots remaining`
+        return `${prop.spots} spots remaining`;
     };
     const dayClass = classNames('day-list__item', {
         'day-list__item--selected': props.selected,
         'day-list__item--full': props.spots === 0
     });
     return (
-        <li className={dayClass} onClick={() => props.setDay(props.name)}>
+        <li className={dayClass} /* onClick={() => props.setDay(props.name)} */>
             <h2 className="text--regular">{props.name}</h2> 
             <h3 className="text--light">{formatSpots(props)}</h3>
         </li>
