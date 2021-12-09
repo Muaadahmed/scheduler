31aaae0db05643c6c5d243ea4a8490d9771447d4
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2021-12-09 17:15:18 -0500

    Populate InterviewList component with InterviewListItem and make sure it passes story book tests

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,11 +2,27 @@ import React from "react";
 
 import "components/InterviewerList.scss"
 
+import InterviewerListItem from "./InterviewerListItem";
+
 export default function InterviewerList(props) {
+  const InterviewerListItems = props.interviewers.map( interviewer => {
+    console.log('interviewer', interviewer);
+    return (
+      <li>
+        <InterviewerListItem 
+          key={interviewer.id} 
+          name={interviewer.name} 
+          avatar={interviewer.avatar} 
+          selected = {interviewer.id === props.interviewer} 
+          setInterviewer = {props.setInterviewer}
+        />
+      </li>
+    );
+  });
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{InterviewerListItems}</ul>
     </section>
   );
 }
