9d7090bc9220472ebe45842d713dcd30c35990a3
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2021-12-10 08:46:57 -0500

    Create Header component and add stories for component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,8 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index.js";
 
+import Header from "components/Appointment/Header.js";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -146,4 +148,5 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm"/>);
+  .add("Appointment with Time", () => <Appointment time="12pm"/>)
+  .add("Header", () => <Header time="12pm"/>)
