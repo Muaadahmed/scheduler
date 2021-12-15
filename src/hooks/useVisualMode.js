2f276f7489af0cf10465b79017424dd419add853
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2021-12-15 12:38:02 -0500

    Update transition function to replace the current mode to take into account situations where a transition is required to happen twice and then set mode back to initial, update back function to make sure that initial mode is always available

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,9 +2,26 @@ import React, {useState} from "react"
 
 export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  function transition (second){
+  const [history, setHistory] = useState([initial]);
+
+  function transition (second, replace=false){
+    if (replace === true) {
+      history.splice(history.indexOf(mode), 1, second);
+      setMode(second);
+      setHistory(history);
+      return;
+    }
+
     setMode(second);
+    history.push(second);
   };
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+    }
+    setMode(history[history.length - 1]);
+  }
   
-  return {mode, transition};
+  return {mode, transition, back};
 };
