446031ceb5698f051e7448dd3775907291c6ba26
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2021-12-15 14:00:21 -0500

    Add several conditional rendering to display intial mode, display form when user clicks on the initial mode, revert back to initial mode when use clicks cancel

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,12 +6,15 @@ import Show from "./Show";
 
 import Empty from "./Empty";
 
+import Form from "./Form";
+
 import "components/Appointment/styles.scss";
 
 import { useVisualMode } from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE ="CREATE";
 
 export default function Appointment(props) {
   const {mode, transition, back} = useVisualMode(
@@ -20,11 +23,20 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          student={props.student}
+          interviewer={props.interviewer}
+          interviewers={[]}
+          onSave={props.onSave}
+          onCancel={() => transition(EMPTY)}
         />
       )}
     </article>
